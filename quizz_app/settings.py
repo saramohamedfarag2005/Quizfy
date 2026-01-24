19ecfc598edfe2c9efb8d7e1717454de2dd73932
saramohamedfarag2005 <134235775+saramohamedfarag2005@users.noreply.github.com>
2026-01-24 22:40:38 +0300

    Fix SMTP settings and add EMAIL_TIMEOUT

--- quizz_app/settings.py
+++ quizz_app/settings.py
@@ -117,6 +117,7 @@ EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "587"))
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "1") == "1"
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
 
 # If you want password reset links to point to your Render domain
 # (helps when generating absolute URLs in emails)
