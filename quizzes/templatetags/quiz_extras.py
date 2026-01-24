83c72f2e1d0c69cde305a8e813bb2b2d5063bf24
saramohamedfarag2005 <134235775+saramohamedfarag2005@users.noreply.github.com>
2026-01-24 12:24:12 +0300

    Fix option_text filter for option1-4 fields

--- quizzes/templatetags/quiz_extras.py
+++ quizzes/templatetags/quiz_extras.py
@@ -2,11 +2,31 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
-def option_text(question, key):
-    if not question or not key:
+def option_text(question, selected):
+    """
+    Works with Question model that has: option1, option2, option3, option4
+    selected might be: "A"/"B"/"C"/"D" OR "1"/"2"/"3"/"4"
+    """
+    if not question or not selected:
         return ""
-    return question.options.get(key, "")
+
+    key = str(selected).strip().upper()
+
+    mapping = {
+        "A": question.option1,
+        "B": question.option2,
+        "C": question.option3,
+        "D": question.option4,
+        "1": question.option1,
+        "2": question.option2,
+        "3": question.option3,
+        "4": question.option4,
+    }
+
+    val = mapping.get(key, "")
+    return val or ""
 
 @register.filter
 def get_item(d, key):
